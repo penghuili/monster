290fc814d280b14a99edc88b7a9ae624d2093a51
penghui li <ichpenghui@gmail.com>
2018-05-08 21:18:53 +0200

    Show only today done (#10)
    * increase the line height of todo item
    
    * show_only_todays_done

--- src/app/todos/todos/todos.component.ts
+++ src/app/todos/todos/todos.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectService, TodoService } from '@app/core';
 import { isOverdue, MonsterStorage, now, Project, Todo, TodoGroup, TodoStatus } from '@app/model';
 import { ROUTES, Unsub } from '@app/static';
-import { addDays, endOfDay } from 'date-fns';
+import { addDays, endOfDay, format } from 'date-fns';
 import { groupBy, keys } from 'ramda';
 import { switchMap } from 'rxjs/operators';
 
@@ -101,15 +101,20 @@ export class TodosComponent extends Unsub implements OnInit {
     this.activeTodosExpectedTime = withTimeTodos.reduce((sum, a) => sum + a.expectedTime, 0);
     this.activeTodoGroup = this.groupTodos(activeTodos);
     this.activeGroups = keys(this.activeTodoGroup);
+
     this.doneTodoGroup = this.groupTodos(
       filtered
-      .filter(a => a.status === TodoStatus.Done || a.status === TodoStatus.WontDo)
+      .filter(a => this.isDoneOnToday(a))
       .sort((a, b) => b.finishAt - a.finishAt)
     );
     this.doneGroups = keys(this.doneTodoGroup);
   }
   private groupTodos(todos: Todo[]): TodoGroup {
     return groupBy(a => a.projectTitle, todos);
+  }
+  private isDoneOnToday(todo: Todo): boolean {
+    return (todo.status === TodoStatus.Done || todo.status === TodoStatus.WontDo) &&
+      format(todo.finishAt, 'YYYYMMDD') === format(now(), 'YYYYMMDD');
   }
   private sortActiveTodo(a: Todo, b: Todo): number {
     return this.scoreTodoForOrder(b) - this.scoreTodoForOrder(a);
