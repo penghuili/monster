6b5ed36c36bfa35d4975768805c028ee9a416c4c
penghui <ichpenghui@gmail.com>
2018-05-08 20:35:46 +0200

    force not drag to diff status

--- src/app/todos/todos/todos.component.ts
+++ src/app/todos/todos/todos.component.ts
@@ -71,8 +71,9 @@ export class TodosComponent extends Unsub implements OnInit {
     if (this.drapGroup === group && dropIndex !== this.dragIndex) {
       const dragged = this.activeTodoGroup[group][this.dragIndex];
       const dropped = this.activeTodoGroup[group][dropIndex];
-      if (dragged && dropped && dragged.status !== TodoStatus.Waiting && dropped.status !== TodoStatus.Waiting) {
-        this.todoService.swap(dragged, dropped);
+      if (dragged && dropped && dragged.status === dropped.status &&
+        dragged.status !== TodoStatus.Waiting && dropped.status !== TodoStatus.Waiting) {
+          this.todoService.swap(dragged, dropped);
       }
     }
     this.dragIndex = undefined;
