6488a596084aec559bcd7baf4964fbd5ea240f93
penghui <ichpenghui@gmail.com>
2018-05-11 20:09:32 +0200

    m85: add used time to todos

--- src/app/settings/process-data/process-data.component.ts
+++ src/app/settings/process-data/process-data.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DbService, NotificationService, ProjectService, TodoService } from '@app/core';
 import { Event, EventType, MonsterEvents, MonsterStorage, Project, Subproject, Todo, TodoStatus } from '@app/model';
 import { combineLatest } from 'rxjs/observable/combineLatest';
+import { fromPromise } from 'rxjs/observable/fromPromise';
 
 @Component({
   selector: 'mst-process-data',
@@ -50,5 +51,56 @@ export class ProcessDataComponent implements OnInit {
   onProcessSubprojects() {
     this.projectService.processSubprojects();
     this.notificationService.sendMessage('process subprojects success :>');
+  }
+
+  onAddUsedTime() {
+    const db = this.dbService.getDB();
+    const transaction = db.transaction('rw', db.todos, db.events, () => {
+      let events: Event[];
+      return db.events
+        .where('type')
+        .equals(EventType.Todo)
+        .toArray()
+        .then(es => {
+          events = es;
+          return db.todos.toArray();
+        })
+        .then(todos => {
+          return [events, todos];
+        });
+    });
+    fromPromise(transaction).subscribe(([events, todos]) => {
+      const startSopEvents = (<Event[]>events).filter(a => a.action === MonsterEvents.StartTodo || a.action === MonsterEvents.StopTodo);
+      if (startSopEvents.length % 2 === 0) {
+        const startEvents = startSopEvents.filter((a, i) => i % 2 === 0);
+        const stopEvents = startSopEvents.filter((a, i) => i % 2 === 1);
+        const todosWithUsedTime = (<Todo[]>todos).map(a => {
+          a.usedTime = this.calcUsedTimeFor(a.id, startEvents, stopEvents);
+          return a;
+        });
+        db.todos.bulkPut(todosWithUsedTime)
+        .then(() => {
+          this.notificationService.sendMessage('add used time success');
+        })
+        .catch(error => {
+          alert(error);
+        });
+      } else {
+        this.notificationService.sendMessage('events should be even');
+      }
+    });
+  }
+
+  private calcUsedTimeFor(todoId: number, startEvents: Event[], stopEvents: Event[]): number {
+    const todoStarts = startEvents.filter(a => a.refId === todoId);
+    const todoStops = stopEvents.filter(a => a.refId === todoId);
+    if (todoStarts.length === todoStops.length) {
+      const milisec = todoStarts.reduce((total, curr, i) => {
+        return total + todoStops[i].createdAt - curr.createdAt;
+      }, 0);
+      return Math.round(milisec / (1000 * 60));
+    } else {
+      return 0;
+    }
   }
 }
