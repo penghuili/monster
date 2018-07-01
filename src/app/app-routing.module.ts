f6d60d26b6a8ccc1f30241c1116c76d1e1c300e1
penghui <ichpenghui@gmail.com>
2018-07-01 13:52:58 +0200

    539: make motivation the default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ROUTES } from '@app/static';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: `${ROUTES.PLANS}/${ROUTES.PROJECTS}`, pathMatch: 'full' },
-  { path: '**', redirectTo: `${ROUTES.PLANS}/${ROUTES.PROJECTS}` }
+  { path: '', redirectTo: `${ROUTES.SETTINGS}/${ROUTES.MOTIVATION}`, pathMatch: 'full' },
+  { path: '**', redirectTo: `${ROUTES.SETTINGS}/${ROUTES.MOTIVATION}` }
 ];
 
 @NgModule({
