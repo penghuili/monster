cb2d1c4c0f7a8c6564a962f5c7f88ad21b1ad7ba
penghui <ichpenghui@gmail.com>
2018-07-03 23:23:32 +0200

    560: go to motivation after a while

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IconsService } from '@app/core';
+import { MonsterStorage, now } from '@app/model';
+import { ROUTES } from '@app/static';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { filter, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'mst-root',
@@ -8,10 +13,26 @@ import { IconsService } from '@app/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private iconsService: IconsService) {
+  constructor(
+    private iconsService: IconsService,
+    private router: Router) {
   }
 
   ngOnInit() {
     this.iconsService.init();
+    this.goToMotivationAfterAWhile();
+  }
+
+  private goToMotivationAfterAWhile() {
+    fromEvent(window, 'focus').pipe(
+      startWith(true),
+      filter(() => {
+        const lastActiveTime = MonsterStorage.get('mst-active-timestamp');
+        return !lastActiveTime || now() - lastActiveTime >= 60 * 60 * 1000;
+      })
+    ).subscribe(() => {
+      MonsterStorage.set('mst-active-timestamp', now());
+      this.router.navigateByUrl(`${ROUTES.SETTINGS}/${ROUTES.MOTIVATION}`);
+    });
   }
 }
